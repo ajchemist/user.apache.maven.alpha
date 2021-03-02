979219434575805450981555fa3513d2aa896993
ajchemist <1694505+aJchemist@users.noreply.github.com>
2021-03-02 15:35:16 +0900

    Fixed user.apache.maven.pom.alpha-test
    pom/gen-pom 수정 후 테스트코드 변경

--- test/user/apache/maven/pom/alpha_test.clj
+++ test/user/apache/maven/pom/alpha_test.clj
@@ -2,25 +2,26 @@
   (:require
    [clojure.test :as test :refer [deftest is are testing]]
    [clojure.java.io :as jio]
-   [user.apache.maven.pom.alpha :refer :all]
+   [user.apache.maven.pom.alpha :as pom]
    ))
 
 
 (deftest main
   (is
-    (= (.getModelVersion (read-pom "pom.xml")) MODEL_VERSION))
+    (= (.getModelVersion (pom/read-pom "pom.xml")) pom/MODEL_VERSION))
 
 
   (is
     (nil?
-      (write-pom
+      (pom/write-pom
         (jio/file (System/getProperty "java.io.tmpdir") "pom.xml")
-        (gen-pom
+        (pom/gen-pom
           "user.apache.maven.alpha"
           "user.apache.maven.alpha"
           "0.0.1-SNAPSHOT"
           '{org.clojure/clojure     {:mvn/version "1.10.0"}
             user.apache.maven.alpha {:local/root "user.apache.maven.alpha"}}
+          nil
           nil
           nil))))
 
@@ -29,12 +30,12 @@
     (==
       (count
         (get-in
-          (model-to-map (read-pom (jio/file (System/getProperty "java.io.tmpdir") "pom.xml")))
+          (pom/model-to-map (pom/read-pom (jio/file (System/getProperty "java.io.tmpdir") "pom.xml")))
           [:dependencies]))
       2))
   )
 
 
 (comment
-  (make-pom-properties 'user.tools.deps.alpha '{:mvn/version "0.0.1-SNAPSHOT"})
+  (pom/make-pom-properties 'user.tools.deps.alpha '{:mvn/version "0.0.1-SNAPSHOT"})
   )
